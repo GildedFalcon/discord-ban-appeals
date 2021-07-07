39528061ec581b9ab5ffbbed80de4e2da948d7ae
GildedFalcon <38820953+GildedFalcon@users.noreply.github.com>
2021-07-06 18:46:27 -0700

    Update discord-code.js

--- func/discord-code.js
+++ func/discord-code.js
@@ -1,11 +1,11 @@
-import java.applet.*;
+/* import java.applet.*;
 import java.awt.*;
 
 public class Main extends Applet{
    public void paint(Graphics g){
       g.drawString("Welcome in Java Applet.",40,20);
    }
-}
+} */
 
 
 
