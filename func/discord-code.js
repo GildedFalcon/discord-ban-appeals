0be234471cdde398a9b8da70c51c4e7550763058
GildedFalcon <38820953+GildedFalcon@users.noreply.github.com>
2021-07-06 18:35:36 -0700

    Update discord-code.js

--- func/discord-code.js
+++ func/discord-code.js
@@ -1,1 +1,18 @@
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+             
+            
+var ms = 5000;
+var func = function () {
+    var randNum = getRandomInt(10000, 99999);    // Gets random number
+    document.getElementById("element_id").innerHTML = randNum;
+};
+
+    
+        
+ setInterval(func, ms); 
+
+
+System.out.println(randNum);
